c51f1a4b3862c1b18c4b2ac0864357adcd74d202
Carlos de Paula <me@carlosedp.com>
2024-01-04 18:01:55 -0300

    Cleaner endpoints under one class

--- src/main/scala/org/acme/GreetingResource.scala
+++ src/main/scala/org/acme/GreetingResource.scala
@@ -4,15 +4,15 @@ import jakarta.ws.rs.{GET, Path, Produces, core}
 import org.jboss.resteasy.reactive.RestQuery
 import jakarta.ws.rs.DefaultValue
 
-@Path("/hello")
+@Path("/")
 class GreetingResource:
+    @Path("/hello")
     @GET
     @Produces(Array[String](core.MediaType.TEXT_PLAIN))
     def hello() = "Hello from RESTEasy Reactive in Scala 3"
 
-// This is an endpoint which greets a name passed via a query parameter "name"
-@Path("/greet")
-class GreetingEndpoint:
+    // This is an endpoint which greets a name or names passed via a query parameter "name"
+    @Path("/greet")
     @GET
     @Produces(Array[String](core.MediaType.TEXT_PLAIN))
     def greeting(@RestQuery @DefaultValue("world") name: Array[String]) =
