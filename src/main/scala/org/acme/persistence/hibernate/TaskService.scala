f126697829af13dc2fd277a1b39926679bb9f8c8
Igor Rumiha <igorrumiha@gmail.com>
2024-01-17 23:35:08 +0100

    Add a basic set of tests

--- src/main/scala/org/acme/persistence/hibernate/TaskService.scala
+++ src/main/scala/org/acme/persistence/hibernate/TaskService.scala
@@ -4,6 +4,8 @@ import jakarta.enterprise.context.ApplicationScoped
 import jakarta.persistence.EntityManager
 import jakarta.transaction.Transactional
 
+import java.util
+
 @ApplicationScoped
 class TaskService(private val em: EntityManager):
 
@@ -11,3 +13,7 @@ class TaskService(private val em: EntityManager):
     def create(task: Task): Task =
         em.persist(task)
         task
+
+    @Transactional
+    def getAll: util.List[Task] =
+        em.createQuery("SELECT t FROM Task t", classOf[Task]).getResultList
