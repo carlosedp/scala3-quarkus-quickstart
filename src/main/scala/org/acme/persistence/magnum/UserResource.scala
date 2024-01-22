7c89c33bdcccde8a39a58bcf15dbfc51c4f55693
Igor Rumiha <igorrumiha@gmail.com>
2024-01-23 00:07:36 +0100

    userPage with Qute templates

--- src/main/scala/org/acme/persistence/magnum/UserResource.scala
+++ src/main/scala/org/acme/persistence/magnum/UserResource.scala
@@ -1,15 +1,26 @@
 package org.acme.persistence.magnum
 
+import io.quarkus.qute.{CheckedTemplate, Template, TemplateInstance}
+import io.smallrye.common.annotation.Blocking
 import jakarta.ws.rs.*
 
+import scala.annotation.static
+import scala.jdk.CollectionConverters.*
+
 @Path("/users")
-class UserResource(private val userService: UserService):
+class UserResource(
+    private val userPage: Template,
+    private val userService: UserService
+  ):
 
     @POST
+    @Consumes(Array("application/json"))
+    @Produces(Array("application/json"))
     def createUser(userCreateCommand: UserCreateUpdateCommand): User =
         userService.create(userCreateCommand)
 
     @GET
+    @Produces(Array("application/json"))
     def getUsers: Vector[User] =
         userService.getAll
 
@@ -27,5 +38,27 @@ class UserResource(private val userService: UserService):
     @Consumes(Array(core.MediaType.APPLICATION_JSON))
     def deleteUser(@PathParam("id") id: Long): Unit =
         userService.deleteUser(id)
+
+
+    @GET
+    @Path("/users-page")
+    @Produces(Array("text/html"))
+    @Blocking
+    def getUsersPage: TemplateInstance =
+        userPage.data("page", UserPageData("Users", userService.getAll.asJava))
+
+    @POST
+    @Path("/users-page")
+    @Consumes(Array("application/x-www-form-urlencoded"))
+    @Produces(Array("text/html"))
+    @Blocking
+    def createUserPage(
+        @FormParam("name") name:         String,
+        @FormParam("email") email:       String,
+        @FormParam("password") password: String,
+      ): TemplateInstance =
+        userService.create(UserCreateUpdateCommand(name, email, password))
+
+        userPage.data("page", UserPageData("Users", userService.getAll.asJava))
 
 end UserResource
